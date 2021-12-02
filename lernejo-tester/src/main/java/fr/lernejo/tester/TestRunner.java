c2153972c68cdf76ec4365fefd44071b17cd437f
Alexis Trupin <link9745@gmail.com>
2021-12-02 09:49:23 +0100

    adding test runner

---
+++ lernejo-tester/src/main/java/fr/lernejo/tester/TestRunner.java
@@ -0,0 +1,49 @@
+package fr.lernejo.tester;
+
+import fr.lernejo.tester.internal.TestClassDescription;
+import fr.lernejo.tester.internal.TestClassDiscoverer;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRunner {
+
+    public static void main(final String[] args) {
+        final List<TestClassDescription> testClassDescriptions = new ArrayList<>();
+        for (final String packageName : args) {
+            testClassDescriptions.addAll(new TestClassDiscoverer(packageName).listTestClasses());
+        }
+
+        final List<Method> methods = new ArrayList<>();
+        for (final TestClassDescription testClassDescription : testClassDescriptions) {
+            methods.addAll(testClassDescription.listTestMethods());
+        }
+
+        long okTests = 0;
+        long koTests = 0;
+        final long allStart = System.currentTimeMillis();
+        for (final Method method : methods) {
+            System.out.println(method.getClass().getName() + "#" + method.getName());
+
+            final long methodStart = System.currentTimeMillis();
+            try {
+                final Object testInstance = method.getClass().getConstructor().newInstance();
+                method.invoke(testInstance);
+                okTests++;
+                System.out.print(" OK");
+            } catch (final InvocationTargetException e) {
+                koTests++;
+                System.out.print(" KO");
+            } catch (final NoSuchMethodException | IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
+            }
+            final long methodTime = System.currentTimeMillis() - methodStart;
+            System.out.println(" " + methodTime + " ms");
+        }
+        final long allTime = System.currentTimeMillis() - allStart;
+        System.out.println();
+        System.out.println("Tests: " + (okTests + koTests) + ", Failed: " + koTests + ", Elapsed: " + allTime + " ms");
+    }
+}
